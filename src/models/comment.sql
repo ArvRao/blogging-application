f7b52609006296d50421a522ae3acdc88dbad55a
arvrao <arvindrao100@gmail.com>
2022-03-21 14:05:55 +0530

    updated project structure

--- src/models/comment.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE comment(
-    id SERIAL PRIMARY KEY,
-    content VARCHAR(255) NOT NULL,
-    postedBy INT,
-    FOREIGN KEY (postedBy) REFERENCES blogger(id),
-    replies TEXT [],
-    
-);
